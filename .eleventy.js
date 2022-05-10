97994f3415818f2c37fc6b39011b87eb3e8ae24d
Eduard <goby145csgo@gmail.com>
2022-05-10 22:14:13 +0200

    добавляет лимит постов в фиде

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = (config) => {
    config.addPassthroughCopy('./src/scripts')
+   config.addFilter('limit', (array, limit) => {
+      return array.slice(0, limit)
+   })
    return {
       dir: {
          input: 'src',
