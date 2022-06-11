309f5d8aa752e0ce930e5a1a1abb544fe2d39ac2
Eduard <goby145csgo@gmail.com>
2022-06-11 11:26:32 +0200

    убирает лишнее

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express()
 const port = process.env.PORT || 1234
 
 app.use(express.static(path.resolve(__dirname, './dist')))
-app.get('*', (req, res) => {
+
+app.get('*', (req, res) =>
    res.sendFile(path.resolve(__dirname, './dist', 'index.html'))
-})
+)
 
 app.listen(port, () => {
    console.log(`Server running at: http://localhost:${port}`)
