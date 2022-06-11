f3a9a5c56b19a3cc41d4cf83f2194e9af656aa8e
Eduard <goby145csgo@gmail.com>
2022-06-11 10:52:30 +0200

    добавляет сервер

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const app = express()
+
+const port = 1234
+
+app.use(express.static(path.resolve(__dirname, './dist')))
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './dist', 'index.html'))
+})
+
+app.listen(port, () => {
+   console.log(`Server running at: http://localhost:${port}`)
+})
