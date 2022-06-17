51748dda30730092e2d945022c69935e9d96db7a
Eduard <goby145csgo@gmail.com>
2022-06-17 11:19:33 +0200

    делает отступы

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
 module.exports = config => {
-   config.addFilter('limit', (array, limit) => {
-      return array.slice(0, limit)
-   })
+   config.addFilter('limit', (array, limit) => array.slice(0, limit))
+
    config.addPassthroughCopy('src/posts')
    config.addPassthroughCopy('src/assets')
    config.addPassthroughCopy('src/scripts')
+
    return {
       dir: {
          input: 'src',
