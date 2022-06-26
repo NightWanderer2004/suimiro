b82bc9e0649966ad1830afb69869f4a71296a92d
Eduard <goby145csgo@gmail.com>
2022-06-26 13:06:11 +0200

    разбивает скрипты на модули

---
+++ src/scripts/settingsColors.js
@@ -0,0 +1,58 @@
+const primaryBtns = document.querySelectorAll('.colorsButton.primary')
+const secondaryBtns = document.querySelectorAll('.colorsButton.secondary')
+
+const feed = [
+   ...document.querySelectorAll('.post'),
+   ...document.querySelectorAll('.hint'),
+]
+const lists = [
+   ...document.querySelectorAll('.postLink'),
+   ...document.querySelectorAll('.hintLink'),
+]
+
+const content = [...feed, ...lists]
+
+const primaryColor = localStorage.getItem('primaryColor')
+const secondaryColor = localStorage.getItem('secondaryColor')
+
+// Устанавливает цвета на элементы
+content.forEach(el => {
+   const isPost =
+      el.classList.contains('post') || el.classList.contains('postLink')
+         ? true
+         : false
+
+   const bgColor = isPost ? primaryColor || 'yellow' : secondaryColor || 'stone'
+   el.classList.add(bgColor)
+})
+
+// Устанавливает класс на кликнутый элемент
+primaryBtns.forEach(el => {
+   if (primaryColor === el.classList[el.classList.length - 1])
+      el.classList.add('active')
+   el.onclick = () => {
+      if (!el.classList.contains('active')) {
+         localStorage.setItem(
+            'primaryColor',
+            el.classList[el.classList.length - 1]
+         )
+      }
+      primaryBtns.forEach(el => el.classList.remove('active'))
+      el.classList.add('active')
+   }
+})
+secondaryBtns.forEach(el => {
+   if (secondaryColor === el.classList[el.classList.length - 1]) {
+      el.classList.add('active')
+   }
+   el.onclick = () => {
+      if (!el.classList.contains('active')) {
+         localStorage.setItem(
+            'secondaryColor',
+            el.classList[el.classList.length - 1]
+         )
+      }
+      secondaryBtns.forEach(el => el.classList.remove('active'))
+      el.classList.add('active')
+   }
+})
