3546db52869e4716d0c9b00e81e1187dc6f42f02
Eduard <goby145csgo@gmail.com>
2022-06-28 11:29:57 +0200

    добавляет авто смену темы

--- src/scripts/darkTheme.js
+++ src/scripts/darkTheme.js
@@ -3,13 +3,18 @@ const themeSwitcher =
 const themeInfo =
    document.querySelector('.darkthemeInfo') || document.createElement('div')
 
-// Проверка, включена ли тёмная тема
-if (localStorage.getItem('theme') === 'dark') {
+const setDarkTheme = () => {
    themeSwitcher.checked = true
    themeInfo.innerHTML = 'Вкл'
    document.body.classList.add('dark')
 }
 
+// Проверка, включена ли тёмная тема
+if (localStorage.getItem('theme') === 'dark') {
+   setDarkTheme()
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+   setDarkTheme()
+}
 // Переклюяает тему
 themeSwitcher.onchange = () => {
    themeSwitcher.checked
