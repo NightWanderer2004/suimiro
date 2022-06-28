c5a37dfb0c6b517d85716ded3350e1f465e02731
Eduard <goby145csgo@gmail.com>
2022-06-28 18:03:24 +0200

    обновляет авто смену темы

--- src/scripts/darkTheme.js
+++ src/scripts/darkTheme.js
@@ -3,26 +3,59 @@ const themeSwitcher =
 const themeInfo =
    document.querySelector('.darkthemeInfo') || document.createElement('div')
 
+const themeSwitcherSystem =
+   document.querySelector('.darkthemeCheckbox.system') ||
+   document.createElement('div')
+const themeInfoSystem =
+   document.querySelector('.darkthemeInfo.system') ||
+   document.createElement('div')
+
+let theme = localStorage.getItem('theme')
+
 const setDarkTheme = () => {
    themeSwitcher.checked = true
    themeInfo.innerHTML = 'Вкл'
    document.body.classList.add('dark')
 }
+const setSystemTheme = () => {
+   themeSwitcherSystem.checked = true
+   if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+      document.body.classList.add('dark')
+}
 
 // Проверка, включена ли тёмная тема
-if (localStorage.getItem('theme') === 'dark') {
-   setDarkTheme()
-} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-   setDarkTheme()
-}
+if (theme === 'dark') setDarkTheme()
+else if (theme === 'system') setSystemTheme()
+
 // Переклюяает тему
 themeSwitcher.onchange = () => {
-   themeSwitcher.checked
-      ? (themeInfo.innerHTML = 'Вкл')
-      : (themeInfo.innerHTML = 'Выкл')
+   if (theme === 'light' || theme === 'system')
+      document.body.classList.add('dark')
+   else document.body.classList.remove('dark')
 
-   const theme = themeSwitcher.checked ? 'dark' : 'light'
-   localStorage.setItem('theme', theme)
+   if (themeSwitcher.checked) {
+      themeInfo.innerHTML = 'Вкл'
+      themeSwitcherSystem.checked = false
+      theme = 'dark'
+   } else {
+      themeInfo.innerHTML = 'Выкл'
+      theme = 'light'
+   }
 
-   document.body.classList.toggle('dark')
+   const currentTheme = themeSwitcher.checked ? 'dark' : 'light'
+   localStorage.setItem('theme', currentTheme)
+}
+themeSwitcherSystem.onchange = () => {
+   if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+      document.body.classList.add('dark')
+   else document.body.classList.remove('dark')
+
+   if (themeSwitcherSystem.checked) {
+      themeSwitcher.checked = false
+      themeInfo.innerHTML = 'Выкл'
+      theme = 'system'
+   } else theme = 'light'
+
+   const currentTheme = themeSwitcherSystem.checked ? 'system' : 'light'
+   localStorage.setItem('theme', currentTheme)
 }
