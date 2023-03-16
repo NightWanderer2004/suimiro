f59cfe3f4c7ad87937b3dbd70f9efc592b338bed
Eduard <goby145csgo@gmail.com>
2023-03-16 11:27:57 +0100

    add path for static

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = config => {
    config.addFilter('limit', (array, limit) => array.slice(0, limit))
 
    config.addPassthroughCopy('src/posts')
+   config.addPassthroughCopy('src/hints')
    config.addPassthroughCopy('src/assets')
    config.addPassthroughCopy('src/scripts')
    config.addPassthroughCopy('src/CNAME')
